2e7f6ef0c406857b2e572360dd666037b7f637ed
unknown <nurlanmu@code.edu.az>
2024-06-10 22:19:01 +0400

    add  fix insturctors and courses

--- MVC-Project/obj/Debug/net6.0/MVC-Project.AssemblyInfo.cs
+++ MVC-Project/obj/Debug/net6.0/MVC-Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b9b06a18a6f4821d27111eb3c99beed3b77d0ab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVC-Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
